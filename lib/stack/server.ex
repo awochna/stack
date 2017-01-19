1f5cd4a0f2ba2a751ee7db083f662052a7c13343
Alex Wynter <awochna@email.arizona.edu>
2017-01-18 20:00:56 -0700

    Implement push interface.

--- lib/stack/server.ex
+++ lib/stack/server.ex
@@ -4,4 +4,8 @@ defmodule Stack.Server do
   def handle_call(:pop, _from, [head|tail]) do
     { :reply, head, tail }
   end
+
+  def handle_cast({:push, item}, current_stack) do
+    { :noreply, [item|current_stack] }
+  end
 end
